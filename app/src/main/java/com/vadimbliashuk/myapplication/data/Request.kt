e1ee185118be7a496fe09d9b6266b3eb98524ffa
Khadgar <blyashuk@list.ru>
2019-12-09 10:24:29 +0300

    Generated Data class(Current Weather)

---
+++ app/src/main/java/com/vadimbliashuk/myapplication/data/Request.kt
@@ -0,0 +1,11 @@
+package com.vadimbliashuk.myapplication.data
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Request(
+    val language: String,
+    val query: String,
+    val type: String,
+    val unit: String
+)
